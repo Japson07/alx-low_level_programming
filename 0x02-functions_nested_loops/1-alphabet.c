12cecde0d41ada50ca18cbab6376da094cbbc90e
Olusegun Ajayi <olusegunajayi09@gmail.com>
2022-07-05 15:18:14 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet -  print alphabets in lower caee
+* Return: Always 0
+*/
+void print_alphabet(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	_putchar(alphabet);
+}
+_putchar('\n');
+}
