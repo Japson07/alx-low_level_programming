ab4da784ed38c0f22b6939d691fa5909f32d5fb7
Olusegun Ajayi <olusegunajayi09@gmail.com>
2022-07-05 15:04:40 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,12 +5,15 @@
 */
 int main(void)
 {
-	char ch[8] = "_putchar";
-	int i;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	for (i = 0; 1 < 8; 1++)
-		_putchar(ch[i]);
-	_putchar('\n');
-
-	return (0);
+return (0);
 }
