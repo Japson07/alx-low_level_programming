7d63af3e508724d22fade0ba31c76dba8ec2e928
Olusegun Ajayi <olusegunajayi09@gmail.com>
2022-07-06 14:35:38 -0700

    README.md

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_times_table - print n times table
+* @n: integer to be read
+* Return: Always 0
+*/
+void print_times_tabled(int n)
+{
+int n;
+
+if (n == '0'; n =='15'; n++)
+
