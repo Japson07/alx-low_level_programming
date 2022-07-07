243c8b1a26b1c857129422a7a78ba97d81efa19a
Olusegun Ajayi <olusegunajayi09@gmail.com>
2022-07-07 13:32:58 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - print numbers from 0 to 9
+* Return: 0
+*/
+
+void print_numbers(void)
+{
+	char = n;
+
+	while (n <= 9)
+	{
+		_putchar('0' + n);
+		n++;
+	}
+	_putchar('\n');
+}
