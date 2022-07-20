89bf000f603b03223618e275a94b12e04818da81
Olusegun Ajayi <olusegunajayi09@gmail.com>
2022-07-20 06:56:38 -0700

    string_length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,11 +2,11 @@
 /**
 * _strlen_recursion - length of string
 * @s: pointer to string
-* Return: 0
+* Return: length of string
 */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-		return (_strlen_recursion(s + 1) + 1);
+	return (_strlen_recursion(s + 1) + 1);
 }
