6bc924a0271a32aae670d9759d71cdf79ff8a4ae
Olusegun Ajayi <olusegunajayi09@gmail.com>
2022-07-25 12:54:53 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,4 @@
-#include "maain.h"
+#include "main.h"
 #include <stdlib.h>
 /**
 * *str_concat - concatenates two strings
@@ -16,25 +16,25 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	/* find length of s1 and s2*/
+
 	while (s1[a] != '\0')
 		a++;
 	while (s2[b] != '\0')
 		b++;
-	/* +1 to knkw the end of string character */
+
 	s = malloc((a * sizeof(char)) + ((b + 1) * sizeof(char)));
 	if (s == NULL)
 		return (NULL);
-	/* add first string to array */
+
 	for (i = 0; s1[i] != '\0'; i++)
 		s[i] = s1[i];
-	/* add second array */
+
 	for (j = 0; s2[j] != '\0'; j++)
 	{
 		s[i] = s2[j];
 		i++;
 	}
-	/* terminate new string */
+
 	s[i] = '\0';
 	return (s);
 }
