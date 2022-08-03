9b687c684b76fbdd608cae669942b34b90cd3b87
Olusegun Ajayi <olusegunajayi09@gmail.com>
2022-08-03 11:17:39 -0700

    3main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,10 +7,10 @@
  * @argv: array of pointers to the arguments
  * Return: Always 0
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int num1, num2;
-	char *op;
+	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
@@ -18,23 +18,23 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
-
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	if(argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	operation = get_op_function(argv[2]);
+
+	if (operation == NULL)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
 
-	printf("%d\n", get_op_func(op)(num1, num2));
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
+	printf("%d\n", operation(num1, num2));
 	return (0);
 }
