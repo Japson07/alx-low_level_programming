6ba2d62b6597eff6430e219a95099d08237e626e
Olusegun Ajayi <olusegunajayi09@gmail.com>
2022-08-04 11:46:52 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,2 +1,17 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+/**
+ * struct format - Data type of a format
+ * @op: Format
+ * @f: Function
+ */
+typedef struct format
+{
+	char *op;
+	void (*f)(va_list all);
+} f;
+#endif
